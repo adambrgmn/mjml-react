36721e56e673dc7c62e77d34d8e7b0f294cbe3ea
Adam Bergman <adam@fransvilhelm.com>
2022-08-10 14:15:52 +0200

    Add handlebars helpers (#2)

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from './extensions';
 export * from './render';
 export * from './types';
 
+export * as Hbs from './handlebars';
+
 export type { MjStyleProps } from './overrides';
 export { MjAll, MjStyle, css } from './overrides';
