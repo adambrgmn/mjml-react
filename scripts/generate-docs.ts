2ed2ecf917f45702840568575c8fbc6b6509e67c
Adam Bergman <adam@fransvilhelm.com>
2023-03-27 10:10:35 +0200

    Update docs generation

--- scripts/generate-docs.ts
+++ scripts/generate-docs.ts
@@ -1,4 +1,23 @@
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+
 import * as cheerio from 'cheerio';
+import { marked } from 'marked';
+
+const OVERRIDES: Record<string, string> = {
+  'mj-attributes': 'mjml-head-html-attributes',
+  'mj-breakpoint': 'mjml-head-breakpoint',
+  'mj-html-attributes': 'mjml-head-html-attributes',
+  'mj-font': 'mjml-head-font',
+  'mj-preview': 'mjml-head-preview',
+  'mj-title': 'mjml-head-title',
+  'mj-social-element': 'mjml-social',
+  'mj-navbar-link': 'mjml-navbar',
+  'mj-accordion-element': 'mjml-accordion',
+  'mj-accordion-text': 'mjml-accordion',
+  'mj-accordion-title': 'mjml-accordion',
+  'mj-carousel-image': 'mjml-carousel',
+};
 
 export type Prop = {
   prop: string;
@@ -9,58 +28,58 @@ export type Prop = {
 
 export type PropsDocumentation = Record<string, { description: string; props: Record<string, Prop> }>;
 
-export async function generatePropsDocumentation(): Promise<PropsDocumentation> {
-  let response = await fetch('https://documentation.mjml.io/');
-  let html = await response.text();
+export function generateComponentDocumentation(
+  name: string,
+): { description: string; props: Record<string, Prop> } | undefined {
+  try {
+    let moduleName = OVERRIDES[name] ?? name.replace(/^mj-/, 'mjml-');
+    let moduleDirectory = path.dirname(require.resolve(moduleName + '/package.json'));
+    let readme = fs.readFileSync(path.join(moduleDirectory, 'README.md'), 'utf-8');
+    let props = extractProps(readme);
 
+    return { description: cleanupReadme(readme), props };
+  } catch (error) {
+    return undefined;
+  }
+}
+
+function extractProps(readme: string) {
+  const html = marked.parse(readme);
   let $ = cheerio.load(html);
-  let content = $('div.content');
+  let props: Record<string, Prop> = {};
 
-  if (!content.is('div')) throw new Error('Can not find page content');
-
-  let headings = $('h1', content);
-  let relevantHeadings = ['components', 'standard head components', 'standard body components'];
-
-  let docs: PropsDocumentation = {};
-
-  for (let heading of headings) {
-    if (!relevantHeadings.includes($(heading).text().toLowerCase())) continue;
-
-    let componentHeadings = $(heading)
-      .nextUntil('h1')
-      .filter((_, el) => el.tagName.toLowerCase() === 'h2');
-
-    for (let componentHeading of componentHeadings) {
-      let componentName = $(componentHeading).text();
-      let table = $(componentHeading)
-        .nextUntil('h2')
-        .filter((_, el) => el.tagName.toLowerCase() === 'table')
-        .first();
-
-      let description = $(componentHeading).next('p[style!="text-align: center;"]');
-
-      if (!table.is('table')) continue;
-
-      docs[componentName] = {
-        description: description.text().trim(),
-        props: {},
-      };
-
-      let rows = $('tr', table);
-      for (let row of rows) {
-        let prop = $('td:nth-child(1)', row).text();
-        let unit = $('td:nth-child(2)', row).text();
-        let description = $('td:nth-child(3)', row).text();
-        let defaultValue = $('td:nth-child(4)', row).text();
-        docs[componentName].props[prop] = {
-          prop,
-          unit,
-          description,
-          defaultValue,
-        };
-      }
-    }
+  let rows = $('tr', $('table'));
+  for (let row of rows) {
+    let prop = $('td:nth-child(1)', row).text();
+    let unit = $('td:nth-child(2)', row).text();
+    let description = $('td:nth-child(3)', row).text();
+    let defaultValue = $('td:nth-child(4)', row).text();
+    props[prop] = {
+      prop,
+      unit,
+      description,
+      defaultValue,
+    };
   }
 
-  return docs;
+  return props;
+}
+
+function cleanupReadme(readme: string) {
+  return readme.replace(/<(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/g, (match) => {
+    let [, tagname] = match.match(/<\/?([\w-]+)/) ?? [];
+    switch (tagname) {
+      case 'b':
+        return '**';
+      case 'em':
+        return '_';
+      case 'aside':
+      case 'p':
+      case 'a':
+      case 'img':
+        return '';
+      default:
+        return match;
+    }
+  });
 }
--- scripts/generate.ts
+++ scripts/generate.ts
@@ -5,7 +5,7 @@ import { components as mjmlComponents } from 'mjml-core';
 import prettier from 'prettier';
 
 import { camelCase, studlyCaps } from '../src/strings';
-import { PropsDocumentation, generatePropsDocumentation } from './generate-docs';
+import { PropsDocumentation, generateComponentDocumentation } from './generate-docs';
 
 // These components require special treatment and are overridden in src/overrides.tsx
 const ignored = ['mj-class', 'mj-all', 'mj-style', 'mj-raw'];
@@ -19,34 +19,33 @@ const specialProps: Record<string, string> = {
   'full-width': `fullWidth?: boolean;`,
 };
 
-(async () => {
-  let sections: string[] = [];
+let sections: string[] = [];
 
-  let imports = `
+let imports = `
 import { createComponent } from './create-component';
 import { MjUnit } from './types';
 `.trim();
 
-  sections.push(imports);
+sections.push(imports);
 
-  let docs = await generatePropsDocumentation();
+for (let [name, component] of Object.entries(mjmlComponents)) {
+  if (ignored.includes(name)) continue;
 
-  for (let [name, component] of Object.entries(mjmlComponents)) {
-    if (ignored.includes(name)) continue;
+  if (component == null) continue;
 
-    if (component == null) continue;
+  let docs = generateComponentDocumentation(name);
 
-    let props = buildProps((component as unknown as any).allowedAttributes ?? {}, docs[name]?.props ?? {});
-    let componentName = studlyCaps(name);
-    let endingTag = (component as unknown as any).endingTag ?? false;
+  let props = buildProps((component as unknown as any).allowedAttributes ?? {}, docs?.props ?? {});
+  let componentName = studlyCaps(name);
+  let endingTag = (component as unknown as any).endingTag ?? false;
 
-    let comments = [
-      (docs[name]?.description ?? '').split('\n').join('\n * '),
-      `This component is${endingTag ? '' : ' not'} an [ending tag](https://documentation.mjml.io/#ending-tags).`,
-      `@link https://documentation.mjml.io/#${name}`,
-    ].filter((c) => c.length > 0);
+  let comments = [
+    (docs?.description ?? '').split('\n').join('\n * '),
+    `This component is${endingTag ? '' : ' not'} an [ending tag](https://documentation.mjml.io/#ending-tags).`,
+    `@link https://documentation.mjml.io/#${name}`,
+  ].filter((c) => c.length > 0);
 
-    let def = `
+  let def = `
 export type ${componentName}Props = ${props};
 
 /**
@@ -55,13 +54,12 @@ export type ${componentName}Props = ${props};
 export let ${componentName} = createComponent<${componentName}Props>('${name}', ${endingTag ? 'true' : 'false'});
   `.trim();
 
-    sections.push(def);
-  }
+  sections.push(def);
+}
 
-  let prettierConfig = JSON.parse(fs.readFileSync('./.prettierrc', 'utf-8'));
-  let code = prettier.format(sections.join('\n\n'), { parser: 'typescript', ...prettierConfig });
-  fs.writeFileSync('./src/components.tsx', code);
-})();
+let prettierConfig = JSON.parse(fs.readFileSync('./.prettierrc', 'utf-8'));
+let code = prettier.format(sections.join('\n\n'), { parser: 'typescript', ...prettierConfig });
+fs.writeFileSync('./src/components.tsx', code);
 
 function buildProps(attributes: Record<string, string>, docs: PropsDocumentation[string]['props']) {
   let rows: string[] = [];
